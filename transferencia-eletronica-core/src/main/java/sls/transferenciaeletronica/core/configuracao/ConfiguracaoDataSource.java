fc3d7126d77989b82cb2c30ef70a74438bcd6549
samuel00 <ifpa.santana@gmail.com>
2019-10-06 18:32:27 -0300

    Adicao das images do projeto para executar com o docker através do fabric8

--- transferencia-eletronica-core/src/main/java/sls/transferenciaeletronica/core/configuracao/ConfiguracaoDataSource.java
+++ transferencia-eletronica-core/src/main/java/sls/transferenciaeletronica/core/configuracao/ConfiguracaoDataSource.java
@@ -47,7 +47,7 @@ public class ConfiguracaoDataSource {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/transferenciaeletronica");
+		dataSource.setUrl("jdbc:mysql://mysqldb:3306/transferenciaeletronica");
 		dataSource.setUsername("root");
 		dataSource.setPassword("root");
 		return dataSource;
