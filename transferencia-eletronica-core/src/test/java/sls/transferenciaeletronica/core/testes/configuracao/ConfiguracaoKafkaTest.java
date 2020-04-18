fd1fe6fb41f8142ca473f33780dae63da93f4101
samuel.santana <samuel.santana@rexturadvance.com.br>
2020-04-18 01:10:46 -0300

    Added Integration Test with Kafka Embedded

---
+++ transferencia-eletronica-core/src/test/java/sls/transferenciaeletronica/core/testes/configuracao/ConfiguracaoKafkaTest.java
@@ -0,0 +1,56 @@
+package sls.transferenciaeletronica.core.testes.configuracao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+@Profile("test")
+public class ConfiguracaoKafkaTest {
+	
+	@Value("${spring.embedded.kafka.brokers}")
+	private String bootstrapServers;
+	
+	
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+		return props;
+	}
+
+	@Bean
+	public KafkaAdmin admin() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+		return new KafkaAdmin(configs);
+	}
+
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+	
+	
+
+
+}
